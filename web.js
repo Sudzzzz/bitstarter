3ab04532c03e2510a674befe0e951b4a9a0c1014
Ubuntu <ubuntu@ip-172-31-10-163.us-west-2.compute.internal>
2013-07-10 23:45:02 +0000

    Web.js modified with index.html

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 var express = require('express');
-
+var fs = require('fs');
+var rfile = "index.html";
+var msg = "";
 var app = express.createServer(express.logger());
 
+function getMsg(readfile){
+    var buff = fs.readFileSync(readfile);
+    return buff.toString('utf8',0,26);
+}
+
 app.get('/', function(request, response) {
-  response.send("Hello World 2!");
+  response.send(getMsg(rfile));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
